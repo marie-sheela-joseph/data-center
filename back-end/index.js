9bdc6ecd6772ad0a3adba42b39dd82caeeba25d2
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-08-07 18:03:20 +0200

    front end - starter files , docker

--- back-end/index.js
+++ back-end/index.js
@@ -19,6 +19,10 @@ app.use(allowCrossDomain);
 app.use('/racks', racksRoutes)
 dotenv.config()
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
+mongoose.connect("mongodb://mongo-db/", { useNewUrlParser: true })
     .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`)))
     .catch((error) => console.log(error))
+
+// mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
+//     .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`)))
+//     .catch((error) => console.log(error))
