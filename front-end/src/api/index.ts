dad179bbab6a715e36f9419c7d5d615381d02ff0
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-08-09 03:22:27 +0200

    back-end-python deploy

--- front-end/src/api/index.ts
+++ front-end/src/api/index.ts
@@ -1,3 +1,5 @@
 export const baseUrl = 'http://127.0.0.1:5000/';
 
 // export const baseUrl = 'https://data-center-sheela.onrender.com/';
+
+// export const baseUrl = 'https://data-center-python.onrender.com/';
