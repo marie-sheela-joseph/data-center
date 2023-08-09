b0429cdc0dcd87b2a674e9cb3b8c0b0f1aa811e0
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-08-09 16:24:35 +0200

    post,patch,delete endpoints

--- back-end-python/app.py
+++ back-end-python/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
 import json
-from bson.objectid import ObjectId
 
 app = Flask(__name__)
 cors = CORS(app) 
@@ -12,60 +11,65 @@ client = MongoClient("mongodb+srv://sheela:pjtPMcuvP8fWiz8Q@cluster1.ctektmw.mon
 db = client["test"]
 collection = db["rackpostmodals"]
 
+@app.route('/')
+def fn():
+    return '<h1>datacentral - racks</h1>',200
+
 @app.route('/racks', methods=['GET'])
-def get_data():        
-    data = list(collection.find({}))    
+def home():        
+    data = list(collection.find({}))
+
     for doc in data:
         doc['_id'] = str(doc['_id'])  
         for device in doc["devices"]:
             device['_id'] = str(device['_id']) 
-    
+
     json_data = json.dumps(data, default=str, indent=2)
     print(json_data)    
     return jsonify({"racks": json.loads(json_data)}), 200
 
-# @app.route('/racks', methods=['POST'])
-# def add_data():
-#     try:
-#         data = request.json        
-#         inserted_id = collection.insert_one(data['rack']).inserted_id
+@app.route('/racks', methods=['POST'])
+def add_data():
+    try:
+        data = request.json        
+        inserted_id = collection.insert_one(data['rack']).inserted_id
 
-#         response = {
-#             'message': 'Data added successfully',
-#             'inserted_id': str(inserted_id)
-#         }
-#         return jsonify(response), 201
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 500
+        response = {
+            'message': 'Data added successfully',
+            'inserted_id': str(inserted_id)
+        }
+        return jsonify(response), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
     
-# @app.route('/racks/<string:rack_id>', methods=['PATCH'])
-# def update_data(rack_id):
-#     try:
-#         data = request.json
-#         rack_data = data['rack']        
-#         rack_id = ObjectId(rack_id)        
-#         collection.update_one({'_id': rack_id}, {'$set': rack_data})
-#         response = {
-#             'message': 'Data updated successfully'
-#         }
-#         return jsonify(response), 200
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 500
+@app.route('/racks/<string:rack_id>', methods=['PATCH'])
+def update_data(rack_id):
+    try:
+        data = request.json
+        rack_data = data['rack']        
+        rack_id = ObjectId(rack_id)        
+        collection.update_one({'_id': rack_id}, {'$set': rack_data})
+        response = {
+            'message': 'Data updated successfully'
+        }
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
     
-# @app.route('/racks/<string:rack_id>', methods=['DELETE'])
-# def delete_data(rack_id):
-#     try:        
-#         rack_id = ObjectId(rack_id)
-#         result = collection.delete_one({'_id': rack_id})
-#         if result.deleted_count == 1:
-#             response = {
-#                 'message': 'Data deleted successfully'
-#             }
-#             return jsonify(response), 200
-#         else:
-#             return jsonify({'message': 'Rack not found'}), 404
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 500
-    
+@app.route('/racks/<string:rack_id>', methods=['DELETE'])
+def delete_data(rack_id):
+    try:        
+        rack_id = ObjectId(rack_id)
+        result = collection.delete_one({'_id': rack_id})
+        if result.deleted_count == 1:
+            response = {
+                'message': 'Data deleted successfully'
+            }
+            return jsonify(response), 200
+        else:
+            return jsonify({'message': 'Rack not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
