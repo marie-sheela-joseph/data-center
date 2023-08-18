3dc8caa5146d51fced763523b0185a3346cb441b
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-08-19 01:54:02 +0200

    racks, devices - GET, POST

---
+++ back-end-django/DataCenterApp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Racks(models.Model):
+    rackId = models.AutoField(primary_key=True)
+    availableVerticalUnits = models.IntegerField()    
+    def __str__(self):
+        return f"Rack {self.rackId}"
+    
+class Devices(models.Model):
+    deviceId = models.AutoField(primary_key=True)    
+    deviceType = models.CharField(max_length=255)
+    modelName = models.CharField(max_length=255)
+    numberOfPorts = models.PositiveIntegerField()
+    verticalSize = models.PositiveIntegerField() 
+    rack = models.ForeignKey(Racks, on_delete=models.CASCADE)       
+    def __str__(self):
+        return f"Device {self.deviceId} in Rack {self.rack.rackId}"
+    
+    
---
+++ back-end-django/DataCenterApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import re_path
+from DataCenterApp import views
+
+urlpatterns = [
+    re_path(r'^rack$', views.rackApi),
+    re_path(r'^device$', views.deviceApi)
+]
